aa0622dc6db4b93b0594552c76af33e4b0b2b411
CincinnatiKid428 <pweaver.psu03@gmail.com>
2025-10-07 03:42:37 -0400

    Exercise 1.1
    - Adding add.py script
    - Adding screenshots of steps 1-5 in exercise (step 6-8 are shown as the GitHub repo, README.me and learning journal PDF)

--- Exercise1.1/add.py
+++ Exercise1.1/add.py
@@ -1,1 +1,5 @@
-
+print("=== This script will add two integers ===")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = a + b
+print(f'The sum of {a} and {b} is {c}')
